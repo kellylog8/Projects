6efb6ccf0400951cd1a25e0ab69652f0aa297b1c
kellyzz <kellylog8@gmail.com>
2021-10-30 17:35:00 +0900

    다형성을 이용하여 여러도형을 같은 list에 저장

--- Project_Drawing/Consol_polymorphism/Program.cs
+++ Project_Drawing/Consol_polymorphism/Program.cs
@@ -10,22 +10,48 @@ namespace Consol_polymorphism
     {
         static void Main(string[] args)
         {
-
+            /*
             Rectangle rect = new Rectangle(20, 10);
             Console.WriteLine("Rectangle Length = " + rect.CalcLength());
             Console.WriteLine("Rectangle Area   = " + rect.CalcArea());
+            Console.WriteLine();
 
             Square square = new Square(10);
             Console.WriteLine("Square Length = " + square.CalcLength());
             Console.WriteLine("Square Area   = " + square.CalcArea());
+            Console.WriteLine();
 
             Triangle triangle = new Triangle(10);
             Console.WriteLine("Triangle Length = " + triangle.CalcLength());
             Console.WriteLine("Triangle Area   = " + triangle.CalcArea());
+            Console.WriteLine();
 
             Circle circle = new Circle(10);
             Console.WriteLine("Circle Length = " + circle.CalcLength());
             Console.WriteLine("Circle Area   = " + circle.CalcArea());
+            Console.WriteLine();
+            */
+
+            List<IShape> list = new List<IShape>();
+
+            Rectangle rect = new Rectangle(20, 10);
+            Square square = new Square(10);
+            Triangle triangle = new Triangle(10);
+            Circle circle = new Circle(10);
+
+            list.Add(rect);
+            list.Add(square);
+            list.Add(triangle);
+            list.Add(circle);
+
+            foreach(IShape shape in list)
+            {
+                Console.WriteLine("Length = " + shape.CalcLength());
+                Console.WriteLine("Area   = " + shape.CalcArea());
+                Console.WriteLine(); 
+            }
+
+
 
         }
     }
